fc23143b967ab089c4ec0f81d4ee54776548908c
KimLizCass <44588353+KimLizCass@users.noreply.github.com>
2019-05-11 16:27:01 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -11,18 +11,15 @@ const NumberofSleeps = document.getElementById("NumberofSleeps");
 const MerryChristmas = document.getElementById("MerryChristmas");
 
 function datediff(first, second) {
-    // Take the difference between the dates and divide by milliseconds per day.
-    // Round to nearest whole number to deal with DST.
-    return Math.round((second-first)/(1000*60*60*24));
+  // Take the difference between the dates and divide by milliseconds per day.
+  // Round to nearest whole number to deal with DST.
+  return Math.round((second-first)/(1000*60*60*24));
 }
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-
   let today = evt.date;
-
   //let today = new Date("2028-12-26")
-
   let year = today.getFullYear()
 
   if (today.getMonth()+1 == 12 && today.getDate() > 25 ) {
@@ -30,7 +27,6 @@ clock.ontick = (evt) => {
   }
 
   let christmas = new Date (`${year}-12-25`);
-
   let daysUntilChristmas = datediff(today, christmas)
   
   if (daysUntilChristmas == 0) {
@@ -38,6 +34,6 @@ clock.ontick = (evt) => {
     SleepsLabel.text = "";
     MerryChristmas.text = "Merry Christmas!";
   } else {
-   NumberofSleeps.text = `${daysUntilChristmas}`; 
+    NumberofSleeps.text = `${daysUntilChristmas}`; 
   }
 }
